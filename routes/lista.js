3ae7d2f4a18f8a4fd6989062ea41c219b6a3b4f5
Aluno IFC <aluno@lab32-pc01.sombrio.ifc.edu.br>
2025-04-29 14:58:58 -0300

    Itens em hbs

--- routes/lista.js
+++ routes/lista.js
@@ -8,7 +8,19 @@ router.get('/', (req, res) => {
       { id: 1, nome: 'Item Alfa', descricao: 'Descrição detalhada do Item Alfa.' },
       { id: 2, nome: 'Item Beta', descricao: 'O segundo item da nossa lista dinâmica.' },
       { id: 3, nome: 'Item Gama', descricao: 'Terceiro e último item exemplo.' },
-      { id: 4, nome: 'Item Delta', descricao: 'Mais um item para testar.' }
+      { id: 4, nome: 'Item Delta', descricao: 'Mais um item para testar.' },
+      { id: 5, nome: 'Item Epsilion', descricao: 'Novo item de teste.'},
+      { id: 6, nome: 'Item Zeta', descricao: 'Novo item de teste.'},
+      { id: 7, nome: 'Item Ita', descricao: 'Novo item de teste.'},
+      { id: 8, nome: 'Item Fita', descricao: 'Novo item de teste.'},
+      { id: 9, nome: 'Item Iota', descricao: 'Novo item de teste.'},
+      { id: 10, nome: 'Item Lota', descricao: 'Novo item de teste.'},
+      { id: 11, nome: 'Item Capa', descricao: 'Novo item de teste.'},
+      { id: 12, nome: 'Item Lambda', descricao: 'Novo item de teste.'},
+      { id: 13, nome: 'Item Mi', descricao: 'Novo item de teste.'},
+      { id: 14, nome: 'Item Ni', descricao: 'Novo item de teste.'},
+      { id: 15, nome: 'Item Fi', descricao: 'Novo item de teste.'}
+      
     ];
   
     // 4. Renderizar a view 'lista.ejs' passando os dados
