d42828e438c9c158e505fbe06220ead143ecaa0b
Aluno IFC <aluno@lab32-pc01.sombrio.ifc.edu.br>
2025-04-29 16:20:42 -0300

    Adicionando o resto do alafabeto

--- routes/lista.js
+++ routes/lista.js
@@ -8,7 +8,27 @@ router.get('/', (req, res) => {
       { id: 1, nome: 'Item Alfa', descricao: 'Descrição detalhada do Item Alfa.' },
       { id: 2, nome: 'Item Beta', descricao: 'O segundo item da nossa lista dinâmica.' },
       { id: 3, nome: 'Item Gama', descricao: 'Terceiro e último item exemplo.' },
-      { id: 4, nome: 'Item Delta', descricao: 'Mais um item para testar.' }
+      { id: 4, nome: 'Item Delta', descricao: 'Mais um item para testar.' },
+      { id: 5, nome: 'Item Épsilon', descricao: 'Descrição detalhada do Item Alfa.' },
+      { id: 6, nome: 'Item Zeta', descricao: 'O segundo item da nossa lista dinâmica.' },
+      { id: 7, nome: 'Item Eta', descricao: 'Terceiro e último item exemplo.' },
+      { id: 8, nome: 'Item Teta', descricao: 'Mais um item para testar.' },
+      { id: 9, nome: 'Item Iota', descricao: 'Descrição detalhada do Item Alfa.' },
+      { id: 10, nome: 'Item Kappa', descricao: 'O segundo item da nossa lista dinâmica.' },
+      { id: 11, nome: 'Item Lambda', descricao: 'Terceiro e último item exemplo.' },
+      { id: 12, nome: 'Item Mu', descricao: 'Mais um item para testar.' },
+      { id: 13, nome: 'Item Ni', descricao: 'Descrição detalhada do Item Alfa.' },
+      { id: 14, nome: 'Item Xi', descricao: 'O segundo item da nossa lista dinâmica.' },
+      { id: 15, nome: 'Item Omicron', descricao: 'Terceiro e último item exemplo.' },
+      { id: 16, nome: 'Item Pi', descricao: 'Mais um item para testar.' },
+      { id: 17, nome: 'Item Rho', descricao: 'Descrição detalhada do Item Alfa.' },
+      { id: 18, nome: 'Item Sigma', descricao: 'O segundo item da nossa lista dinâmica.' },
+      { id: 19, nome: 'Item Tau', descricao: 'Terceiro e último item exemplo.' },
+      { id: 20, nome: 'Item Ípsilon', descricao: 'Mais um item para testar.' },
+      { id: 21, nome: 'Item Fi', descricao: 'Descrição detalhada do Item Alfa.' },
+      { id: 22, nome: 'Item Chi', descricao: 'O segundo item da nossa lista dinâmica.' },
+      { id: 23, nome: 'Item Psi', descricao: 'Terceiro e último item exemplo.' },
+      { id: 24, nome: 'Item Ômega', descricao: 'Mais um item para testar.' }
     ];
   
     // 4. Renderizar a view 'lista.ejs' passando os dados
