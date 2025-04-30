9711e6facbe07f89c50f3a1b81bc595b8f80e5b9
Aluno IFC <aluno@lab32-pc01.sombrio.ifc.edu.br>
2025-04-30 09:00:34 -0300

    Versão comleta da lista

--- routes/lista.js
+++ routes/lista.js
@@ -5,11 +5,31 @@ var router = express.Router();
 router.get('/', (req, res) => {
     // 3. Dados dinâmicos (nosso vetor de elementos)
     const meusItens = [
-      { id: 1, nome: 'Item Alfa', descricao: 'Descrição detalhada do Item Alfa.' },
-      { id: 2, nome: 'Item Beta', descricao: 'O segundo item da nossa lista dinâmica.' },
-      { id: 3, nome: 'Item Gama', descricao: 'Terceiro e último item exemplo.' },
-      { id: 4, nome: 'Item Delta', descricao: 'Mais um item para testar.' }
-    ];
+    { "id": 1, "nome": "Alfa", "descricao": "Primeira letra do alfabeto grego." },
+    { "id": 2, "nome": "Beta", "descricao": "Segunda letra do alfabeto grego." },
+    { "id": 3, "nome": "Gama", "descricao": "Terceira letra do alfabeto grego." },
+    { "id": 4, "nome": "Delta", "descricao": "Quarta letra do alfabeto grego." },
+    { "id": 5, "nome": "Épsilon", "descricao": "Quinta letra do alfabeto grego." },
+    { "id": 6, "nome": "Zeta", "descricao": "Sexta letra do alfabeto grego." },
+    { "id": 7, "nome": "Eta", "descricao": "Sétima letra do alfabeto grego." },
+    { "id": 8, "nome": "Teta", "descricao": "Oitava letra do alfabeto grego." },
+    { "id": 9, "nome": "Iota", "descricao": "Nona letra do alfabeto grego." },
+    { "id": 10, "nome": "Kappa", "descricao": "Décima letra do alfabeto grego." },
+    { "id": 11, "nome": "Lambda", "descricao": "Décima primeira letra do alfabeto grego." },
+    { "id": 12, "nome": "Mi", "descricao": "Décima segunda letra do alfabeto grego." },
+    { "id": 13, "nome": "Ni", "descricao": "Décima terceira letra do alfabeto grego." },
+    { "id": 14, "nome": "Xi", "descricao": "Décima quarta letra do alfabeto grego." },
+    { "id": 15, "nome": "Omicron", "descricao": "Décima quinta letra do alfabeto grego." },
+    { "id": 16, "nome": "Pi", "descricao": "Décima sexta letra do alfabeto grego." },
+    { "id": 17, "nome": "Rho", "descricao": "Décima sétima letra do alfabeto grego." },
+    { "id": 18, "nome": "Sigma", "descricao": "Décima oitava letra do alfabeto grego." },
+    { "id": 19, "nome": "Tau", "descricao": "Décima nona letra do alfabeto grego." },
+    { "id": 20, "nome": "Ípsilon", "descricao": "Vigésima letra do alfabeto grego." },
+    { "id": 21, "nome": "Fí", "descricao": "Vigésima primeira letra do alfabeto grego." },
+    { "id": 22, "nome": "Khi", "descricao": "Vigésima segunda letra do alfabeto grego." },
+    { "id": 23, "nome": "Psi", "descricao": "Vigésima terceira letra do alfabeto grego." },
+    { "id": 24, "nome": "Ômega", "descricao": "Última letra do alfabeto grego." }
+    ]
   
     // 4. Renderizar a view 'lista.ejs' passando os dados
     // O segundo argumento de res.render() é um objeto.
