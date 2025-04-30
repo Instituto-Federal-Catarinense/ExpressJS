da8a0987799366d1998ef377e3517d870e7a56ea
luisafrancca <luisafrancca@gmail.com>
2025-04-30 09:01:45 -0300

    Alfabeto Grego HBS

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var exphbs = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var clientesRouter = require('./routes/clientes');
 var listaRouter = require('./routes/lista');
 
-
 var app = express();
 
 // view engine setup
+app.engine('hbs', exphbs.engine({
+  extname: 'hbs',
+  defaultLayout: 'layout',
+  layoutsDir: path.join(__dirname, 'views')
+}));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,7 +32,6 @@ app.use('/users', usersRouter);
 app.use('/clientes', clientesRouter);
 app.use('/lista', listaRouter);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -35,11 +39,9 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
