051107a79e2c87f6a2185d748786229f63ff256c
Aluno IFC <aluno@lab32-pc01.sombrio.ifc.edu.br>
2025-05-07 09:31:51 -0300

    lista de users com imagem

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var clientesRouter = require('./routes/clientes');
 var listaRouter = require('./routes/lista');
+var usersRouter = require('./routes/users');
 
 
 var app = express();
