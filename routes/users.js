25431b4da0da4b7af5c46591ddcff6b85d61b50c
Isabele <isabelebitencourtherr@gmail.com>
2025-05-09 15:08:15 -0300

    Versão com o usuário

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// Rota principal para renderizar nossa lista
+router.get('/', (req, res) => {
+  // 3. Dados dinâmicos (nosso vetor de elementos)
+  const meusUsuarios = [
+    { id: 1, nome: 'Vôlei', descricao: 'Descrição detalhada do Item Alfa.' },
+    { id: 2, nome: 'Futsal', descricao: 'O segundo item da nossa lista dinâmica.' },
+    { id: 3, nome: 'Badminton', descricao: 'Terceiro e último item exemplo.' },
+    { id: 4, nome: 'Handebol', descricao: 'Mais um item para testar.' }
+    { id: 4, nome: 'Tênis', descricao: 'Outro item para testar.' }
+    { id: 4, nome: 'Futbol', descricao: 'Mais um item para testar.' }
+  ];
 
-/* GET users listing. */
-router.get('/joedio', function(req, res, next) {
-  res.send('Olá, Joédio!');
+  // 4. Renderizar a view 'lista.ejs' passando os dados
+  // O segundo argumento de res.render() é um objeto.
+  // As chaves desse objeto (ex: 'titulo', 'itensParaView') se tornam
+  // variáveis disponíveis dentro do template EJS.
+  res.render('usuarios', {
+    titulo: 'Minha Lista Dinâmica',
+    itensParaView: meusItens // Passando o array para a view
+  });
 });
 
 module.exports = router;
