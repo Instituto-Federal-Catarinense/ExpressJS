ffb53232cc60361cbb13073f83c4bdfd78565854
Gabriel Trajano <gabris.trajano@gmail.com>
2025-05-13 16:37:55 -0300

    Lista de Produtos

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var clientesRouter = require('./routes/clientes');
 var listaRouter = require('./routes/lista');
-
+var produtoRouter = require('./routes/produto');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/clientes', clientesRouter);
 app.use('/lista', listaRouter);
-
+app.use('/produto', produtoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
