ac4fe5c443e3dbd44d9ad4f78288c6b42aacea3a
Aluno IFC <aluno@lab32-pc01.sombrio.ifc.edu.br>
2025-05-13 16:53:34 -0300

    Lista de Produtos

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var clientesRouter = require('./routes/clientes');
 var listaRouter = require('./routes/lista');
-
+var produtosRouter = require('./routes/produtos')
 
 var app = express();
 
@@ -22,11 +22,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/clientes', clientesRouter);
 app.use('/lista', listaRouter);
-
+app.use('/produtos', produtosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
