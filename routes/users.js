33fe6ca885d3dd6487d54ec7e9d13965bdeb9922
Isabele <isabelebitencourtherr@gmail.com>
2025-05-20 21:49:17 -0300

    Versão com imagem e preço

--- routes/users.js
+++ routes/users.js
@@ -8,15 +8,12 @@ router.get('/', (req, res) => {
     { id: 1, nome: 'Vôlei', descricao: 'Descrição detalhada do Item Alfa.' },
     { id: 2, nome: 'Futsal', descricao: 'O segundo item da nossa lista dinâmica.' },
     { id: 3, nome: 'Badminton', descricao: 'Terceiro e último item exemplo.' },
-<<<<<<< HEAD
     { id: 4, nome: 'Handebol', descricao: 'Mais um item para testar.' },
     { id: 5, nome: 'Tênis', descricao: 'Outro item para testar.' },
-    { id: 6, nome: 'Futbol', descricao: 'Mais um item para testar.' }
-=======
-    { id: 4, nome: 'Handebol', descricao: 'Mais um item para testar.' }
-    { id: 4, nome: 'Tênis', descricao: 'Outro item para testar.' }
+    { id: 6, nome: 'Futbol', descricao: 'Mais um item para testar.' },
+    { id: 4, nome: 'Handebol', descricao: 'Mais um item para testar.' },
+    { id: 4, nome: 'Tênis', descricao: 'Outro item para testar.' },
     { id: 4, nome: 'Futbol', descricao: 'Mais um item para testar.' }
->>>>>>> 3612a0c9ed3dfd6b85e608ac23e43f821445feab
   ];
 
   // 4. Renderizar a view 'lista.ejs' passando os dados
